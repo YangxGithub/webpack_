4b71772ca077404d978082938c27e44287a1a775
wyc <bemyself008@163.com>
2021-04-21 18:58:42 +0800

    新增

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,8 @@ const htmlWebpackPlugin = new HtmlWebpackPlugin({
   filename: 'index.html',
   favicon: path.resolve('public/favicon.ico')
 })
+const MiniCssExtractPlugin = require('mini-css-extract-plugin') //  从js中提取css
+process.env.NODE_ENV = 'development' 
 module.exports = {
   mode: 'development',
   entry: path.join(__dirname, "./src/js/index.js"),
@@ -17,12 +19,52 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin(),
-    htmlWebpackPlugin
+    htmlWebpackPlugin,
+    new MiniCssExtractPlugin({
+      // 对输出的css文件指定存放路径以及命名
+      filename: 'css/style.css'
+    })
   ],
   module: {
     rules: [{
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: [
+          // 创建style便签 将样式放入
+          // 'style-loader',
+          // 这个loader 取代style-loader,作用:将js的css提取成为单独的css文件
+          {
+            loader:MiniCssExtractPlugin.loader,
+            options:{
+              publicPath: '../'
+            } // 解决使用MiniCssExtractPlugin后css的背景图出不来问题
+          },
+          // 将css文件整合到js文件中
+          'css-loader',
+          // css 兼容处理 postcss-loader postcss-preset-env
+          /**
+           * "browserslist":{
+              "development":[
+                "last 1 chrome version", 兼容最近版本的1个谷歌浏览器
+                "last 1 firefox version",
+                "last 1 safari version"
+              ],
+              "production":[
+                ">0.2%", 兼容98%的浏览器
+                "not dead", 死掉的浏览器不要
+                "not op_mini all" Open迷你不要
+              ]
+            }
+           */
+          {
+            loader:'postcss-loader',
+            options:{
+              ident:'postcss',
+              plugins:() => {
+                require('postcss-preset-env')()
+              }
+            }
+          }
+        ]
       },
       {
         test: /\.(htm|html)$/i,
@@ -35,7 +77,8 @@ module.exports = {
           options: {
             limit: 13000,
             esModule: false,
-            name: 'images/[name].[hash:8].[ext]'
+            name: '[hash:8].[ext]',
+            outputPath:'imgs' // 指定图片输出到images文件夹下
           }
         }]
       },
@@ -44,7 +87,8 @@ module.exports = {
         exclude:/\.(html|css|png|jpg|js)/,
         loader:'file-loader',
         options:{
-          name:'[hash:10].[ext]'
+          name:'[hash:10].[ext]',
+          outputPath:'other' // 指定资源输出到other目录下
         }
       }
     ]
